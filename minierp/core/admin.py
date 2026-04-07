ba91bf0355b5b77f31b8446b4aae618cb49b530a
Andrea Fernandez de la Rosa <a28707@svalero.com>
2026-04-07 14:12:23 +0200

    Añado views y urls protegidas

--- minierp/core/admin.py
+++ minierp/core/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Cliente, Producto, Estado
 
-admin.site.register(Cliente)
-admin.site.register(Producto)
-admin.site.register(Estado)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nif', 'nombre', 'email')
+    search_fields = ('nif', 'nombre', 'email')
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sku', 'nombre', 'precio', 'stock')
+    search_fields = ('sku', 'nombre')
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre')
--- minierp/core/views.py
+++ minierp/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+from .forms import ClienteForm, ProductoForm
+from .models import Cliente, Producto
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+class ProductoCreateView(CreateView):
+    model = Producto
+    form_class = ProductoForm
+    template_name = 'core/producto_form.html'
+    success_url = reverse_lazy('producto_add')
+
+class ClienteCreateView(CreateView):
+    model = Cliente
+    form_class = ClienteForm
+    template_name = 'core/cliente_form.html'
+    success_url = reverse_lazy('cliente_add')
