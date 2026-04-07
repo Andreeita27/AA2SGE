c397e9990c47af08eac52ede548275a3d92728fa
Andrea Fernandez de la Rosa <a28707@svalero.com>
2026-04-07 17:27:38 +0200

    Configuro Django Rest Framework

--- minierp/minierp/settings.py
+++ minierp/minierp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     'ventas',
     'crm',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAUL_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
